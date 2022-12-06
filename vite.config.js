5448aa72cb951bf2597536d6d597afa0a59c2817
Emiliano <e.acevedo.felip@gmail.com>
2022-12-05 21:00:44 -0300

    feat - pwa config for vite plugin

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA()
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Mi carrito',
+        short_name: 'Carrito',
+        description: 'Administrador de carrito de compras',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'app-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'app-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'app-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
 })
